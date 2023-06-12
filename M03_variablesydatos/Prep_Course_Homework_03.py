064193060edd53f7b73e2d9ebb2739ecacb68ea6
Alejo Peralta Falconi <51446143+AlejoPeralta05@users.noreply.github.com>
2023-06-12 16:50:27 -0300

    first commit

--- M03_variablesydatos/Prep_Course_Homework_03.py
+++ M03_variablesydatos/Prep_Course_Homework_03.py
@@ -7,14 +7,15 @@
 
 # In[7]:
 
-
+a = 12
+print (a)
 
 
 # 2) Imprimir el tipo de dato de la constante 8.5
 
 # In[3]:
 
-
+type(8.5)
 
 
 
@@ -23,13 +24,14 @@
 # In[8]:
 
 
-
+type(a)
 
 
 # 4) Crear una variable que contenga tu nombre
 
 # In[2]:
 
+mi_nombre = "Alejo Nicolas Peralta Falconi"
 
 
 
@@ -37,7 +39,7 @@
 
 # In[3]:
 
-
+n_complejo = 6 + 6
 
 
 
@@ -46,7 +48,7 @@
 # In[4]:
 
 
-
+type (n_complejo)
 
 
 # 7) Crear una variable que contenga el valor del número Pi redondeado a 4 decimales
@@ -61,7 +63,8 @@ pi = 3.1416
 
 # In[3]:
 
-
+var1 = "True"
+var2 = True
 
 
 
@@ -69,8 +72,7 @@ pi = 3.1416
 
 # In[5]:
 
-
-
+print("La variable 1 es de tipo ", type(var1), "y la variable 2 es de tipo ", type(var2))
 
 
 # 10) Asignar a una variable, la suma de un número entero y otro decimal
@@ -78,7 +80,7 @@ pi = 3.1416
 # In[1]:
 
 
-
+var_suma = 6 + 4.5
 
 
 # 11) Realizar una operación de suma de números complejos
@@ -86,15 +88,17 @@ pi = 3.1416
 # In[2]:
 
 
-
-
+a = 2 + 4j
+b= 4 + 2j
+print(a + b)
 
 # 12) Realizar una operación de suma de un número real y otro complejo
 
 # In[4]:
 
 
-
+c = a + 2.65
+print (c)
 
 
 # 13) Realizar una operación de multiplicación
@@ -102,14 +106,14 @@ pi = 3.1416
 # In[5]:
 
 
-
+print( 4 * 4 )
 
 
 # 14) Mostrar el resultado de elevar 2 a la octava potencia
 
 # In[6]:
 
-
+print( 4**4)
 
 
 # 15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
@@ -117,7 +121,8 @@ pi = 3.1416
 # In[8]:
 
 
-
+a = 27/5 
+print(a)
 
 
 # 16) De la división anterior solamente mostrar la parte entera
@@ -125,7 +130,7 @@ pi = 3.1416
 # In[9]:
 
 
-
+print (27 // 5) 
 
 
 # 17) De la división de 27 entre 4 mostrar solamente el resto
@@ -133,14 +138,14 @@ pi = 3.1416
 # In[1]:
 
 
-
+27 % 5
 
 
 # 18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
 
 # In[2]:
 
-
+6 * 4 + 3
 
 
 
@@ -149,14 +154,16 @@ pi = 3.1416
 # In[3]:
 
 
-
+var1 = "Buenos"
+var2 = "Aires"
+print( var1 + var2 )
 
 
 # 20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
 
 # In[4]:
 
-
+2 == "2"
 
 
 
@@ -165,7 +172,7 @@ pi = 3.1416
 # In[11]:
 
 
-
+2 == int("2")
 
 
 # 22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
@@ -173,7 +180,7 @@ pi = 3.1416
 # In[12]:
 
 
-
+a = float("3,8")
 
 
 # 23) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido
@@ -181,14 +188,16 @@ pi = 3.1416
 # In[15]:
 
 
-
+a = -1
+a = -3
+print (a)
 
 
 # 24) Realizar la operacion 1 << 2 ¿Por qué da ese resultado? ¿Qué es el sistema de numeración binario?
 
 # In[29]:
 
-
+1 << 2
 
 
 
@@ -198,7 +207,7 @@ pi = 3.1416
 
 
 
-
+2 + "2"
 
 
 # 26) Realizar una operación válida entre valores de tipo entero y string
@@ -206,4 +215,9 @@ pi = 3.1416
 # In[30]:
 
 
+var1 = 'este texto se repite '
+var2 = 3
+print(var1 * var2 + str(var2) + ' veces')
 
+
+# %%
--- M04_flujosdecontrol/Prep_Course_Homework_04.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04.py
@@ -7,31 +7,44 @@
 
 # In[4]:
 
-
-
+a = 10
+if (a < 0):
+    print("La variable es menor a 0")
+elif (a > 0):
+    print("La variable es mayor a 0")
+else:
+    print("La variable es igual a 0")
 
 
 # 2) Crear dos variables y un condicional que informe si son del mismo tipo de dato
 
 # In[5]:
 
-
-
-
+a = 2
+b = "hola"
+if(type(a) == type(b)):
+    print("Las variables son del mismo tipo de dato")
+else:
+    print("Las variables son de distinto tipo de dato")
 
 # 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
 
 # In[7]:
 
 
-
+for i in range (1, 21):
+    if i % 2 == 0:
+        print("El numero", str (i), " es par")
+    else:
+        print("El numero", str(i), " es impar" )
 
 
 # 4) En un ciclo for mostrar para los valores entre 0 y 5 el resultado de elevarlo a la potencia igual a 3
 
 # In[9]:
 
-
+for i in range(0, 6):
+    print("Valor:", str(i), " Elevado a la tercera potencia:", str(i ** 3))
 
 
 
@@ -39,7 +52,10 @@
 
 # In[10]:
 
-
+n = 12 
+for i in range(0 , n):
+    pass
+print(i)
 
 
 
@@ -47,23 +63,38 @@
 
 # In[33]:
 
-
-
-
+n = 5 
+if (type (n) == int):
+    if(n > 0):
+        factorial = n
+        while (n > 2):
+            n = n - 1
+            factorial = factorial + n 
+        print("El factorial es", factorial)
+    else:
+        print("La variable no es un entero")
 
 # 7) Crear un ciclo for dentro de un ciclo while
 
 # In[38]:
 
-
-
-
+n = 0
+while(n < 5):
+    n += 1
+    for i in range(1,n):
+        print('Ciclo while nro ' + str(n))
+        print('Ciclo for nro ' + str(i))
 
 # 8) Crear un ciclo while dentro de un ciclo for
 
 # In[3]:
 
-
+n = 5
+for i in range(1, n):
+    while(n < 5):
+        n -= 1
+        print('Ciclo while nro ' + str(n))
+        print('Ciclo for nro ' + str(i))
 
 
 
@@ -71,14 +102,37 @@
 
 # In[54]:
 
-
+tope_rango=30
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
 
 
 # 10) ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
 
 # In[55]:
 
-
+tope_rango=30
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
 
 
 
@@ -86,31 +140,98 @@
 
 # In[56]:
 
+tope_rango = 30
+ciclos_sin_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_sin_break += 1
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_sin_break))
 
 
 
 # In[57]:
-
+tope_rango = 30
+ciclos_con_break = 0
+ciclos_sin_break = 30
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_con_break += 1
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_con_break))
+print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
 
 
 
 # 12) Si la cantidad de números que se evalúa es mayor a treinta, esa optimización crece?
 
 # In[58]:
-
+tope_rango=100
+ciclos_sin_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_sin_break += 1
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_sin_break))
 
 
 
 
 # In[59]:
-
+tope_rango = 100
+ciclos_con_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_con_break += 1
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_con_break))
+print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
 
 
 
 # 13) Aplicando continue, armar un ciclo while que solo imprima los valores divisibles por 12, dentro del rango de números de 100 a 300
 
 # In[62]:
-
+n = 99
+while(n <= 300):
+    n += 1
+    if (n % 12 != 0):
+        continue
+    print(n, ' es divisible por 12')
 
 
 
@@ -118,6 +239,23 @@
 # 14) Utilizar la función **input()** que permite hacer ingresos por teclado, para encontrar números primos y dar la opción al usario de buscar el siguiente
 
 # In[73]:
+n = 1
+sigue = 1
+primo = True
+while (sigue == 1):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+        print('¿Desea encontrar el siguiente número primo?')
+        if (input() != '1'):
+            print('Se finaliza el proceso')
+            break
+    else:
+        primo = True
+    n += 1
 
 
 
@@ -125,6 +263,13 @@
 # 15) Crear un ciclo while que encuentre dentro del rango de 100 a 300 el primer número divisible por 3 y además múltiplo de 6
 
 # In[75]:
+n = 100 
+while(n<=300):
+    if (n % 68 == 0):
+        print("El numero es: ", str(n))
+        break
+    n += 1
 
 
 
+# %%
